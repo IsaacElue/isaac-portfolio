b3e34b8aeeb02ecc5f58e579e0819d7eef3769ee
Isaac Elue <your-school-email@example.com>
2025-11-07 05:07:26 +0000

    sc

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <body
         className={cn(
           inter.className,
